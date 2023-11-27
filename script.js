f5b76452dbbc8e1a9d480004dce6083fb43708a1
Saurabh pande <93812514+Sourabhpande532@users.noreply.github.com>
2023-11-27 11:10:57 +0530

    feat - input-output-processing[Js]

--- script.js
+++ script.js
@@ -3,6 +3,29 @@ const luckyNumber = document.querySelector("#lucky-number");
 const checkButtonNumber = document.querySelector("#check-number");
 const outputBox = document.querySelector("#output-box");
 
-checkButtonNumber.addEventListener("click", () => {
-  console.log(dateOfBirth.value, luckyNumber.value);
-});
+function compareValue(sum, luckyNumber) {
+  if (sum % luckyNumber === 0) {
+    outputBox.innerText = "Congratulation 🎉🎊, Your Birthday is lucky";
+  } else {
+    outputBox.innerText = "Oops😔! sadly Your birthday Not Lucky";
+  }
+}
+
+function checkBirthdayIsLucky() {
+  let dob = dateOfBirth.value;
+  let sum = calculateSum(dob);
+  dob && luckyNumber.value
+    ? compareValue(sum, luckyNumber.value)
+    : (outputBox.innerText = "Both field is required");
+}
+
+function calculateSum(dob) {
+  dob = dob.replaceAll("-", "");
+  let sumUp = 0;
+  for (let i = 0; i < dob.length; i++) {
+    sumUp = sumUp + Number(dob.charAt(i));
+  }
+  return sumUp;
+}
+
+checkButtonNumber.addEventListener("click", checkBirthdayIsLucky);
